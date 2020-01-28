b6925b0c4ea4ada1250d115ae8dd90c937013d45
Tatsuro <contacts@tatsuro.dev>
2020-01-28 16:51:39 +0900

    v-ifでdataのbookable1,2があるときのみrender 24

--- laravelbnb/public/js/app.js
+++ laravelbnb/public/js/app.js
@@ -1916,9 +1916,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
-//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   components: {
@@ -1940,9 +1937,6 @@ __webpack_require__.r(__webpack_exports__);
   created: function created() {
     var _this = this;
 
-    console.log('created');
-    console.log(this.bookable1);
-    console.log(this.bookable2);
     setTimeout(function () {
       _this.bookable1 = {
         title: 'Cheap Villa',
@@ -1952,36 +1946,8 @@ __webpack_require__.r(__webpack_exports__);
         title: 'Cheap Villa 2',
         content: 'A very cheap villa 2'
       };
-      _this.bookable3 = {
-        title: 'Expensive Villa',
-        content: 'A very cheap villa 3'
-      };
-    }, 5000); // dataに定義したreactive dataを変更すると即座に反映される
-
-    setTimeout(function () {
-      console.log('First change');
-      _this.bookable1.title = 'You will see this!';
-    }, 8000); // dataに定義せずに後でdata objectに代入してもreactiveにならない、ここではbookable3を12000ms後に変更しているがreactiveに反映されない
-
-    setTimeout(function () {
-      console.log('Second change');
-      _this.bookable3.title = 'You wont see this!';
-    }, 12000);
-  } //   beforeMount() {
-  //     console.log('before mount')
-  //   },
-  // child componentのmountedが呼ばれてからparent componentのmountedが呼ばれる
-  //   mounted() {
-  //     console.log('mounted')
-  //   }
-  // destroyは表示されるcomponentが変化する時にcall
-  //   beforeDestroy() {
-  //     console.log('before destroy')
-  //   },
-  //   destroyed() {
-  //     console.log('destroyed')
-  //   }
-
+    }, 2000);
+  }
 });
 
 /***/ }),
@@ -37493,29 +37459,25 @@ var render = function() {
   return _c(
     "div",
     [
-      _c("bookable-list-item", {
-        attrs: {
-          "item-title": _vm.bookable1.title,
-          "item-content": _vm.bookable1.content,
-          price: 1000
-        }
-      }),
+      _vm.bookable1
+        ? _c("bookable-list-item", {
+            attrs: {
+              "item-title": _vm.bookable1.title,
+              "item-content": _vm.bookable1.content,
+              price: 1000
+            }
+          })
+        : _vm._e(),
       _vm._v(" "),
-      _c("bookable-list-item", {
-        attrs: {
-          "item-title": _vm.bookable2.title,
-          "item-content": _vm.bookable2.content,
-          price: 1500
-        }
-      }),
-      _vm._v(" "),
-      _c("bookable-list-item", {
-        attrs: {
-          "item-title": _vm.bookable3.title,
-          "item-content": _vm.bookable3.content,
-          price: 1500
-        }
-      })
+      _vm.bookable2
+        ? _c("bookable-list-item", {
+            attrs: {
+              "item-title": _vm.bookable2.title,
+              "item-content": _vm.bookable2.content,
+              price: 1500
+            }
+          })
+        : _vm._e()
     ],
     1
   )
--- laravelbnb/resources/js/bookables/Bookables.vue
+++ laravelbnb/resources/js/bookables/Bookables.vue
@@ -4,18 +4,15 @@
     <!-- kebab-caseで渡したpropsは、PascaleCaseで使用する -->
     <!-- propsの型を指定するにはv-bindでquoteの中をjavascript式にして、渡したい型の文字列、数字、配列、オブジェクト等を指定する -->
     <bookable-list-item
+      v-if="bookable1"
       :item-title="bookable1.title"
       :item-content="bookable1.content"
       :price="1000"
     ></bookable-list-item>
     <bookable-list-item
+      v-if="bookable2"
       :item-title="bookable2.title"
       :item-content="bookable2.content"
-      :price="1500"
-    ></bookable-list-item>
-    <bookable-list-item
-      :item-title="bookable3.title"
-      :item-content="bookable3.content"
       :price="1500"
     ></bookable-list-item>
   </div>
@@ -41,9 +38,6 @@ export default {
   //   },
   // apiでdataを取得するような場合、時間が多少かかるのでcreated等でできるだけ早くdataを取得するようにするとbetter
   created() {
-    console.log('created')
-    console.log(this.bookable1)
-    console.log(this.bookable2)
     setTimeout(() => {
       this.bookable1 = {
         title: 'Cheap Villa',
@@ -53,36 +47,8 @@ export default {
         title: 'Cheap Villa 2',
         content: 'A very cheap villa 2'
       }
-      this.bookable3 = {
-        title: 'Expensive Villa',
-        content: 'A very cheap villa 3'
-      }
-    }, 5000)
-    // dataに定義したreactive dataを変更すると即座に反映される
-    setTimeout(() => {
-      console.log('First change')
-      this.bookable1.title = 'You will see this!'
-    }, 8000)
-    // dataに定義せずに後でdata objectに代入してもreactiveにならない、ここではbookable3を12000ms後に変更しているがreactiveに反映されない
-    setTimeout(() => {
-      console.log('Second change')
-      this.bookable3.title = 'You wont see this!'
-    }, 12000)
+    }, 2000)
   }
-  //   beforeMount() {
-  //     console.log('before mount')
-  //   },
-  // child componentのmountedが呼ばれてからparent componentのmountedが呼ばれる
-  //   mounted() {
-  //     console.log('mounted')
-  //   }
-  // destroyは表示されるcomponentが変化する時にcall
-  //   beforeDestroy() {
-  //     console.log('before destroy')
-  //   },
-  //   destroyed() {
-  //     console.log('destroyed')
-  //   }
 }
 </script>
 
