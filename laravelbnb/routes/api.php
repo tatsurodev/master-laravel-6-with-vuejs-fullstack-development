04ba847950e0a7592523d1478f5a174f316221a9
Tatsuro <contacts@tatsuro.dev>
2020-01-29 00:33:51 +0900

    api routeでbookables用indexをjsonを返す 32

--- laravelbnb/routes/api.php
+++ laravelbnb/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Bookable;
 use Illuminate\Http\Request;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('bookables', function (Request $request) {
+    // 配列、collectionを返すことでjsonに変換される
+    return Bookable::all();
 });
