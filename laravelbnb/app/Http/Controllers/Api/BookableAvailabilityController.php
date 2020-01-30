258e69a30ce0a9f28a4e637fa79f4670c562dbfe
Tatsuro <contacts@tatsuro.dev>
2020-01-30 19:19:07 +0900

    BookableAbilityControllerにfrom, toのvalidation追加 53

--- laravelbnb/app/Http/Controllers/Api/BookableAvailabilityController.php
+++ laravelbnb/app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -15,6 +15,12 @@ class BookableAvailabilityController extends Controller
      */
     public function __invoke(Request $request)
     {
-        dd('this is working');
+        // 通常はvalidation error時にerror msgとredirectが返ってくるので、headerでAccept application/jsonを指定してerror msgをjsonで受け取るようにpostman等で指定する
+        $data = $request->validate([
+            'from' => 'required|date_format:Y-m-d|after_or_equal:now',
+            'to' => 'required:date_format:Y-m-d|after_or_equal:from',
+        ]);
+
+        dd($data);
     }
 }
