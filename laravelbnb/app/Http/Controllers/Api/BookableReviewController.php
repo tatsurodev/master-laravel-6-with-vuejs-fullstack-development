a71a62a65069b5c6f8f12c9a5f1b8df69e4868c3
Tatsuro <contacts@tatsuro.dev>
2020-02-03 00:50:07 +0900

    BookableReviewIndexResourceを作成してreviewの必要な項目のみreturn 64
    art make:resource BookableReviewIndexResource

--- laravelbnb/app/Http/Controllers/Api/BookableReviewController.php
+++ laravelbnb/app/Http/Controllers/Api/BookableReviewController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Bookable;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\BookableReviewIndexResource;
 use Illuminate\Http\Request;
 
 class BookableReviewController extends Controller
@@ -17,6 +18,9 @@ class BookableReviewController extends Controller
     public function __invoke($id, Request $request)
     {
         $bookable = Bookable::findOrFail($id);
-        return $bookable->reviews()->latest()->get();
+
+        return BookableReviewIndexResource::collection(
+            $bookable->reviews()->latest()->get()
+        );
     }
 }
