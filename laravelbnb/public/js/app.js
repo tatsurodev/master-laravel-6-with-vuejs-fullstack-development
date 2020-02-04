f3cdd6e4c71c923617ad7b322878631858a070a8
Tatsuro <contacts@tatsuro.dev>
2020-02-04 10:12:04 +0900

    StarRating componentのlogic作成、星表示 68

--- laravelbnb/public/js/app.js
+++ laravelbnb/public/js/app.js
@@ -2060,6 +2060,14 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
 // import moment from 'moment'
 /* harmony default export */ __webpack_exports__["default"] = ({
   props: {
@@ -2232,15 +2240,29 @@ __webpack_require__.r(__webpack_exports__);
   },
   computed: {
     halfStar: function halfStar() {
-      return true;
+      var fraction = Math.round(this.rating - Math.floor(this.rating)) * 100; //   console.log(fraction)
+
+      return fraction > 0 && fraction < 50;
     },
     fullStars: function fullStars() {
-      return 3;
+      // > 4.5 = 5 stars
+      // 4.3 = 4 and half
+      return Math.round(this.rating);
     },
     emptyStars: function emptyStars() {
-      return 2;
+      // if rating would be 1.9, ceil(1.9) = 2, 5 - 2 = 3, render 3 empty stars
+      return 5 - Math.ceil(this.rating);
     }
-  }
+  } //   created() {
+  //     const numbers = [0.9, 4.0, 4.4, 4.6, 4.9]
+  //     numbers.forEach(n => {
+  //       console.log(`round for ${n} is ${Math.round(n)}`)
+  //       console.log(`floor for ${n} is ${Math.floor(n)}`)
+  //       console.log(`ceil for ${n} is ${Math.ceil(n)}`)
+  //       console.log('=================================')
+  //     })
+  //   }
+
 });
 
 /***/ }),
@@ -56145,57 +56167,82 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", { staticStyle: { padding: "1.25rem" } }, [
-    _c(
-      "h6",
-      { staticClass: "text-uppercase text-secondary font-weight-bolder pt-4" },
-      [_vm._v("Review List")]
-    ),
-    _vm._v(" "),
-    _vm.loading
-      ? _c("div", [_vm._v("Loading...")])
-      : _c(
-          "div",
-          _vm._l(_vm.reviews, function(review, index) {
-            return _c(
-              "div",
-              { key: index, staticClass: "border-bottom d-none d-md-block" },
-              [
-                _c("div", { staticClass: "row pt-4" }, [
-                  _c("div", { staticClass: "col-md-6" }, [
-                    _vm._v("Piotr Jura")
+  return _c(
+    "div",
+    { staticStyle: { padding: "1.25rem" } },
+    [
+      _c(
+        "h6",
+        {
+          staticClass: "text-uppercase text-secondary font-weight-bolder pt-4"
+        },
+        [_vm._v("Review List")]
+      ),
+      _vm._v(" "),
+      _c("star-rating", { attrs: { rating: 4.0 } }),
+      _vm._v(" "),
+      _c("star-rating", { attrs: { rating: 4.4 } }),
+      _vm._v(" "),
+      _c("star-rating", { attrs: { rating: 4.5 } }),
+      _vm._v(" "),
+      _c("star-rating", { attrs: { rating: 4.6 } }),
+      _vm._v(" "),
+      _c("star-rating", { attrs: { rating: 0.9 } }),
+      _vm._v(" "),
+      _c("star-rating", { attrs: { rating: 3.2 } }),
+      _vm._v(" "),
+      _c("star-rating", { attrs: { rating: 3.5 } }),
+      _vm._v(" "),
+      _c("star-rating", { attrs: { rating: 3.7 } }),
+      _vm._v(" "),
+      _vm.loading
+        ? _c("div", [_vm._v("Loading...")])
+        : _c(
+            "div",
+            _vm._l(_vm.reviews, function(review, index) {
+              return _c(
+                "div",
+                { key: index, staticClass: "border-bottom d-none d-md-block" },
+                [
+                  _c("div", { staticClass: "row pt-4" }, [
+                    _c("div", { staticClass: "col-md-6" }, [
+                      _vm._v("Piotr Jura")
+                    ]),
+                    _vm._v(" "),
+                    _c(
+                      "div",
+                      { staticClass: "col-md-6 d-flex justify-content-end" },
+                      [
+                        _vm._v(
+                          "\n          " +
+                            _vm._s(review.rating) +
+                            "\n          "
+                        ),
+                        _c("star-rating", { attrs: { rating: review.rating } })
+                      ],
+                      1
+                    )
                   ]),
                   _vm._v(" "),
-                  _c(
-                    "div",
-                    { staticClass: "col-md-6 d-flex justify-content-end" },
-                    [
-                      _vm._v(
-                        "\n          " + _vm._s(review.rating) + "\n          "
-                      ),
-                      _c("star-rating")
-                    ],
-                    1
-                  )
-                ]),
-                _vm._v(" "),
-                _c("div", { staticClass: "row" }, [
-                  _c("div", { staticClass: "col-md-12" }, [
-                    _vm._v(_vm._s(_vm._f("fromNow")(review.created_at)))
+                  _c("div", { staticClass: "row" }, [
+                    _c("div", { staticClass: "col-md-12" }, [
+                      _vm._v(_vm._s(_vm._f("fromNow")(review.created_at)))
+                    ])
+                  ]),
+                  _vm._v(" "),
+                  _c("div", { staticClass: "row pb-4" }, [
+                    _c("div", { staticClass: "col-md-12" }, [
+                      _vm._v(_vm._s(review.content))
+                    ])
                   ])
-                ]),
-                _vm._v(" "),
-                _c("div", { staticClass: "row pb-4" }, [
-                  _c("div", { staticClass: "col-md-12" }, [
-                    _vm._v(_vm._s(review.content))
-                  ])
-                ])
-              ]
-            )
-          }),
-          0
-        )
-  ])
+                ]
+              )
+            }),
+            0
+          )
+    ],
+    1
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
