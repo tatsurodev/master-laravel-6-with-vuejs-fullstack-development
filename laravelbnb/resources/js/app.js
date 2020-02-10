4d16a452409591a1af68881743308556f2abf41c
Tatsuro <contacts@tatsuro.dev>
2020-02-11 00:58:57 +0900

    FatalError component作成 86

--- laravelbnb/resources/js/app.js
+++ laravelbnb/resources/js/app.js
@@ -6,6 +6,7 @@ import Index from './Index'
 import moment from 'moment'
 
 import StarRating from './shared/components/StarRating'
+import FatalError from './shared/components/FatalError'
 
 window.Vue = require('vue')
 
@@ -14,6 +15,7 @@ Vue.use(VueRouter)
 Vue.filter('fromNow', value => moment(value).fromNow())
 
 Vue.component('star-rating', StarRating)
+Vue.component('fatal-error', FatalError)
 
 const app = new Vue({
   el: '#app',
